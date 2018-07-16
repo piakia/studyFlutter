155e864d6c4be0e28333e05a7156e8da22da1785
piapia <towserpai@163.com>
2018-07-16 16:24:38 +0800

    create project

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+void main() {}
+
+class InfoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget titleSection = Container(
+        padding: const EdgeInsets.all(32.0),
+        child: Row(
+          children: [],
+        ));
+    return null;
+  }
+}
